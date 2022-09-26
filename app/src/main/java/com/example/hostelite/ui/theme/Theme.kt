e1ca3632f5a45fa2189d0d6d1fac880af90d87b7
RitwikSingh28 <itsritwik26@gmail.com>
2022-09-26 07:37:53 +0530

    Change systembar color to white

--- app/src/main/java/com/example/hostelite/ui/theme/Theme.kt
+++ app/src/main/java/com/example/hostelite/ui/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -15,7 +17,7 @@ private val DarkColorPalette = darkColors(
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
 
     /* Other default colors to override
     background = Color.White,
@@ -30,6 +32,11 @@ private val LightColorPalette = lightColors(
 @Composable
 fun HosteliteTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = LightColorPalette
+    val systemUiController = rememberSystemUiController()
+
+    systemUiController.setSystemBarsColor(
+        color = Color.White
+    )
 
     MaterialTheme(
         colors = colors,
