e95178665d9bb9c4aadc9a4f6c9fc6943971a830
RitwikSingh28 <itsritwik26@gmail.com>
2022-09-26 15:59:20 +0530

    implement sign-in page (student&admin)

--- app/src/main/java/com/example/hostelite/MainActivity.kt
+++ app/src/main/java/com/example/hostelite/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.example.hostelite.landing_pages.Login
 import com.example.hostelite.ui.theme.HosteliteTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    BoardingPage()
+                    Login()
                 }
             }
         }
